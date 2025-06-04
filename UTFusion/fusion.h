8827614eb800e8c89b4ebb755a4f7ff604996d68
hatef2003 <hatef.1382@gmail.com>
2025-06-04 22:51:49 +0330

    add: methods to the fusion

--- UTFusion/fusion.h
+++ UTFusion/fusion.h
@@ -1,6 +1,7 @@
 #ifndef FUSION_H
 #define FUSION_H
 
+#include <QIODevice>
 #include <QObject>
 
 class Fusion : public QObject
@@ -8,8 +9,13 @@ class Fusion : public QObject
     Q_OBJECT
 public:
     explicit Fusion(QObject *parent = nullptr);
-
+public slots:
+    void fuse(void *radarData, void *camData);
 signals:
+    void DataReady();
+
+private:
+    QIODevice *m_outputDevice;
 };
 
 #endif // FUSION_H
