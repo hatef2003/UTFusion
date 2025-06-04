f9e9a7de119d3e8350424f6e2c08bd42b196f5aa
hatef2003 <hatef.1382@gmail.com>
2025-06-04 22:58:52 +0330

    add: implement guid for fuse function

--- UTFusion/fusion.cpp
+++ UTFusion/fusion.cpp
@@ -3,3 +3,10 @@
 Fusion::Fusion(QObject *parent)
     : QObject{parent}
 {}
+
+void Fusion::fuse(void *radarData, void *camData)
+{
+    char *result;
+    // TODO Fusino
+    m_outputDevice->write(result);
+}
