47024fcb7b96c180e7e99f2a4cdbceb4681774eb
arianfiroozi <arianfiroozi3@gmail.com>
2025-06-05 13:52:02 +0330

    fix: rm tiny thing

--- UTFusion/datacontainer.cpp
+++ UTFusion/datacontainer.cpp
@@ -5,7 +5,6 @@ DataContainer::DataContainer(QObject *parent)
 {
     //TODO ..
     mu = new QMutex();
-    m_readIndex&=0;
     m_buffers[0] = std::make_unique<Buffer>();
     m_buffers[1] = std::make_unique<Buffer>();
 }
