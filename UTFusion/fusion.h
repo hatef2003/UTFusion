b09943af4011c9c5f438f3f7c84ebb84af37bb4a
ArianFiroozi <arianfiroozi3@gmail.com>
2025-07-30 19:12:33 +0330

    feat: may gods of memory management forgive me for what i have done

--- UTFusion/fusion.h
+++ UTFusion/fusion.h
@@ -8,6 +8,16 @@ class Fusion
 {
     void find_closest_point_in_box(uint16_t,uint16_t,uint16_t,uint16_t);
     float min_cam_distance = std::numeric_limits<float>::infinity(), min_radar_distance = std::numeric_limits<float>::infinity();
+
+    const uint8_t radar_fov0[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov1[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov2[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov3[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov4[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov5[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov6[IMG_LEN*IMG_WIDTH][2];
+    const uint8_t radar_fov7[IMG_LEN*IMG_WIDTH][2];
+
 public:
     Fusion(uint8_t);
     uint8_t len_imgboxes;
