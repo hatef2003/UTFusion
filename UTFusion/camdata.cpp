30ea1d4169ddf0c37820f86a43dc86b7ef04cd3d
ArianFiroozi <arianfiroozi3@gmail.com>
2025-07-30 14:00:01 +0330

    feat: find closes distance in img

--- UTFusion/camdata.cpp
+++ UTFusion/camdata.cpp
@@ -1,3 +1,29 @@
 #include "camdata.h"
+#include <limits>
+
+uint8_t CamData::find_closest_box()
+{
+    float min_cam_distance = std::numeric_limits<float>::infinity(), min_radar_distance = std::numeric_limits<float>::infinity();
+    for (int i = 0; i < IMG_LEN; i++)
+        for (int j = 0; j < IMG_WIDTH; j++)
+        {
+            if (cam_img[i][j] < min_cam_distance)
+                min_cam_distance = cam_img[i][j];
+            if (radar_img[i][j] < min_radar_distance)
+                min_radar_distance = radar_img[i][j];
+        }
+}
 
 CamData::CamData() {}
+
+void CamData::merge_images()
+{
+    if (len_imgboxes >= MAX_IMAGE_BOXES)
+        throw 1;
+
+    find_closest_box();
+    
+    for (int i = 0; i < this->len_imgboxes; i++)
+    {
+    }
+}
--- UTFusion/camdata.h
+++ UTFusion/camdata.h
@@ -1,14 +1,20 @@
 #ifndef CAMDATA_H
 #define CAMDATA_H
 #include<vector>
-
+#define MAX_IMAGE_BOXES 16
+#define IMG_LEN 640
+#define IMG_WIDTH 640
 class CamData
 {
+    uint8_t find_closest_box();
 public:
     CamData();
-    int len_imgboxes;
-    unsigned int imgboxes[10][4]; //TODO: change the number of bounding boxes
-    float img[640][640]; //TODO: change the image size
+    uint8_t len_imgboxes;
+    unsigned int imgboxes[MAX_IMAGE_BOXES][4]; //TODO: change the number of bounding boxes
+    float radar_img[IMG_LEN][IMG_WIDTH]; //TODO: change the image size
+    float cam_img[IMG_LEN][IMG_WIDTH]; //TODO: change the image size
+
+    void merge_images();
 };
 
 #endif // CAMDATA_H
