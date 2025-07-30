9bafff7ad655de544df118e209e3b9d1995d0abf
ArianFiroozi <arianfiroozi3@gmail.com>
2025-07-30 14:43:04 +0330

    feat: merge by pooling box by box

--- UTFusion/camdata.cpp
+++ UTFusion/camdata.cpp
@@ -1,11 +1,13 @@
 #include "camdata.h"
 #include <limits>
 
-uint8_t CamData::find_closest_box()
+void CamData::find_closest_point_in_box(uint16_t ulx, uint16_t uly, uint16_t brx, uint16_t bry) // up left x and y, bottom right x and y
 {
-    float min_cam_distance = std::numeric_limits<float>::infinity(), min_radar_distance = std::numeric_limits<float>::infinity();
-    for (int i = 0; i < IMG_LEN; i++)
-        for (int j = 0; j < IMG_WIDTH; j++)
+    min_cam_distance = std::numeric_limits<float>::infinity();
+    min_radar_distance = std::numeric_limits<float>::infinity();
+
+    for (int i = ulx; i < brx; i++)
+        for (int j = uly; j < bry; j++)
         {
             if (cam_img[i][j] < min_cam_distance)
                 min_cam_distance = cam_img[i][j];
@@ -14,16 +16,27 @@ uint8_t CamData::find_closest_box()
         }
 }
 
-CamData::CamData() {}
+CamData::CamData(uint8_t _len_imageboxes) {
+    len_imgboxes = _len_imageboxes;
+}
 
 void CamData::merge_images()
 {
     if (len_imgboxes >= MAX_IMAGE_BOXES)
         throw 1;
 
-    find_closest_box();
-    
+        
     for (int i = 0; i < this->len_imgboxes; i++)
     {
+        uint16_t ulx = this->imgboxes[i][0], uly = this->imgboxes[i][1], brx = this->imgboxes[i][2], bry = this->imgboxes[i][3];
+        find_closest_point_in_box(ulx, uly, brx,bry);
+        if (min_radar_distance < min_cam_distance) continue;
+        
+        float box_ratio = (float)min_radar_distance/(float)min_cam_distance;
+        for (int i = ulx; i < brx; i++)
+            for (int j = uly; j < bry; j++)
+                if (cam_img[i][j] < min_cam_distance)
+                    cam_img[i][j]*=box_ratio;
+        
     }
 }
--- UTFusion/camdata.h
+++ UTFusion/camdata.h
@@ -6,11 +6,12 @@
 #define IMG_WIDTH 640
 class CamData
 {
-    uint8_t find_closest_box();
+    void find_closest_point_in_box(uint16_t,uint16_t,uint16_t,uint16_t);
+    float min_cam_distance = std::numeric_limits<float>::infinity(), min_radar_distance = std::numeric_limits<float>::infinity();
 public:
-    CamData();
+    CamData(uint8_t);
     uint8_t len_imgboxes;
-    unsigned int imgboxes[MAX_IMAGE_BOXES][4]; //TODO: change the number of bounding boxes
+    uint16_t imgboxes[MAX_IMAGE_BOXES][4]; //TODO: change the number of bounding boxes
     float radar_img[IMG_LEN][IMG_WIDTH]; //TODO: change the image size
     float cam_img[IMG_LEN][IMG_WIDTH]; //TODO: change the image size
 
