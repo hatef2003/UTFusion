71ddeae1e96b5b76e1a2970a2dea7749b6ea80e0
ArianFiroozi <arianfiroozi3@gmail.com>
2025-08-01 00:19:40 +0330

    fix: distance_calc, a lot of redundancies since i wanted to preserve everything

---
+++ Fusion-reimagined/distance_calculator.cpp
@@ -0,0 +1,35 @@
+#include "distancecalculator.h"
+#include <cmath>
+
+DistanceCalculator::DistanceCalculator()
+{}
+
+float DistanceCalculator::distance(float p1[3], float p2[3])
+{
+    return distance(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+}
+
+float DistanceCalculator::distance(float x1, float y1, float z1,
+                                   float x2, float y2, float z2)
+{
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float dz = z2 - z1;
+
+    return std::sqrt(dx*dx + dy*dy + dz*dz);
+}
+
+float DistanceCalculator::distanceSquared(float p1[3], float p2[3])
+{
+    
+    float dx = p2[0] - p1[0];
+    float dy = p2[1] - p1[1];
+    float dz = p2[2] - p1[2];
+
+    return dx*dx + dy*dy + dz*dz;
+}
+
+float DistanceCalculator::calculateDistance(float p1[3], float p2[3])
+{
+    return distance(p1, p2);
+}
---
+++ Fusion-reimagined/distance_calculator.h
@@ -0,0 +1,16 @@
+#ifndef DISTANCECALCULATOR_H
+#define DISTANCECALCULATOR_H
+
+// static class for calculations
+class DistanceCalculator
+{
+public:
+    static float distance(float p1[3], float p2[3]);
+    static float distance(float x1, float y1, float z1,
+                          float x2, float y2, float z2);
+    static float distanceSquared(float p1[3], float p2[3]);
+    
+    static float calculateDistance(float p1[3], float p2[3]);
+};
+
+#endif // DISTANCECALCULATOR_H
