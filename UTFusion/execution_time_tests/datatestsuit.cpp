57c51b5302c7106340a94d0d32cabcce2f86f931
ArianFiroozi <arianfiroozi3@gmail.com>
2025-08-05 16:12:59 +0330

    feat: add data container suit, a possible bug found

--- UTFusion/execution_time_tests/datatestsuit.cpp
+++ UTFusion/execution_time_tests/datatestsuit.cpp
@@ -5,8 +5,14 @@ DataTestsuit::DataTestsuit() {}
 
 void DataTestsuit::setup()
 {
+    setup_container();
+    setup_buf();
+}
+
+void DataTestsuit::setup_buf()
+{
     QImage img1(640, 480, QImage::Format_RGB32);
-    img1.fill(Qt::white);
+    img1.fill(Qt::black);
     QImage img2(640, 480, QImage::Format_RGB32);
     img2.fill(Qt::white);
 
@@ -14,9 +20,37 @@ void DataTestsuit::setup()
     this->buf.addCam({&img1, &img2, 2000});
 }
 
+void DataTestsuit::setup_container()
+{
+    qint64 timestamp = QDateTime::currentMSecsSinceEpoch();
+
+    Buffer::RadarData radarData = {timestamp, 1, 2, 3, 4, 5, 6};
+    QImage* img1 = new QImage(640, 480, QImage::Format_RGB32);
+    QImage* img2 = new QImage(640, 480, QImage::Format_RGB32);
+    img1->fill(Qt::black);
+    img2->fill(Qt::white);
+    Buffer::CameraData camData = {img1, img2, timestamp};
+
+    qint64 timestamp2 = timestamp + 15;
+
+    Buffer::RadarData radarData2 = {timestamp2, 1, 2, 3, 4, 5, 6};
+    Buffer::CameraData camData2 = {img1, img2, timestamp2};
+
+    this->container.newRadarData(radarData);
+    this->container.newCamData(camData);
+
+    this->container.newRadarData(radarData2);
+    this->container.newCamData(camData2);
+}
+
 void DataTestsuit::test_buffer_isValid()
 {
     this->buf.isSync();
+}
+
+void DataTestsuit::test_container_getData()
+{
+    this->container.getData();
 }
 
 QStringList DataTestsuit::run_suit()
@@ -24,12 +58,19 @@ QStringList DataTestsuit::run_suit()
     QStringList results;
 
     results << "DataTestSuit:";
-    results << benchmark("setup", [this]() {
-        this->setup();
+    results << benchmark("setup_buf", [this]() {
+        this->setup_buf();
+    }, 1);
+    results << benchmark("setup_container", [this]() {
+        this->setup_container();
     }, 1);
 
     results << benchmark("buffer_isValid", [this]() {
         this->test_buffer_isValid();
+    }, 5);
+
+    results << benchmark("container_getData", [this]() {
+        this->test_container_getData();
     }, 5);
 
     for (const QString& line : results) {
--- UTFusion/execution_time_tests/datatestsuit.h
+++ UTFusion/execution_time_tests/datatestsuit.h
@@ -7,10 +7,15 @@
 class DataTestsuit : TestSuit
 {
     Buffer buf;
+    DataContainer container;
 public:
     DataTestsuit();
     void setup();
+    void setup_container();
+    void setup_buf();
+
     void test_buffer_isValid();
+    void test_container_getData();
     QStringList run_suit();
 };
 
